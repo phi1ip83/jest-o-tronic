21948882e2aa09a1f9bedde750830b0c388ad001
phi1ip83 <philip.dossantos@hotmail.com>
2025-12-12 00:26:25 -0500

    added logger, readme dockerhub update, logger implemented in main file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 // The Client and Intents are destructured from discord.js, since it exports an object by default. Read up on destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 const { Client, IntentsBitField } = require('discord.js');
 
-const prefix = process.env.prefix || '!';
+const logger = require('./logger');
+
+const prefix = process.env.PREFIX || '!';
 const client = new Client({
   intents: [
     IntentsBitField.Flags.GuildMessages,
@@ -14,19 +16,21 @@ const client = new Client({
 });
 
 client.on('clientReady', () => {
-  console.log('I am ready! ');
+  logger.info('I am ready! ');
 });
 
 client.on('messageCreate', (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   if (message.content === `${prefix}help`) {
+    logger.info('Help command invoked by: ' + message.author.username);
     message.channel.send(`Available commands: ${prefix}ping, ${prefix}help`);
   }
 
   if (message.content.startsWith(`${prefix}ping`)) {
+    logger.info('Ping command invoked by: ' + message.author.username);
     message.channel.send('pong!');
   }
 });
 
-client.login(process.env.token);
+client.login(process.env.TOKEN);
